3cc32e3e3e817c0d12fe59a12382068a95efd24c
soarflat <soarflat777@gmail.com>
2017-11-29 00:48:41 +0900

    update test for ChromeStorage

--- test/common/ChromeStorage.test.js
+++ test/common/ChromeStorage.test.js
@@ -1,3 +1,4 @@
+import sinon from 'sinon';
 import chrome from 'sinon-chrome';
 import assert from 'assert';
 import ChromeStorage from '../../src/common/ChromeStorage';
@@ -5,20 +6,30 @@ import ChromeStorage from '../../src/common/ChromeStorage';
 describe('ChromeStorage', () => {
   before(() => {
     global.chrome = chrome;
-    chrome.storage.local.get.yields([{
-      title: 'new String() と String() の違い',
-      href: 'https://qiita.com/y_ito/items/33e303877f7eb44b15ff',
-    }]);
+
+    // sinon-chromeをimportしているため
+    // chrome.storage.local.getなどはsinon stubsに置き換わっている
+    chrome.storage.local.get
+      .withArgs('dmmCollections')
+      .yields([{
+        title: 'new String() と String() の違い',
+        href: 'https://qiita.com/y_ito/items/33e303877f7eb44b15ff',
+      }]);
   });
 
   it('should returns item data', () => {
+    let called = false;
+
     ChromeStorage.get({
       keys: 'dmmCollections',
       callback: (items) => {
         const item = items[0];
         assert.equal(item.title, 'new String() と String() の違い');
         assert.equal(item.href, 'https://qiita.com/y_ito/items/33e303877f7eb44b15ff');
+        called = true;
       },
     });
+
+    assert.ok(called, 'called callback function');
   });
 });
