37cec2a8385d5d78ff80c1f80ad67f2ac4b1e98c
soarflat <soarflat777@gmail.com>
2017-12-05 00:11:23 +0900

    [update] ストレージのデータをviewに表示

--- extension/js/settings.js
+++ extension/js/settings.js
@@ -4323,11 +4323,15 @@ if (false) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Histories_vue__ = __webpack_require__(21);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Histories_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Histories_vue__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_1e20208e_hasScoped_false_node_modules_vue_loader_lib_selector_type_template_index_0_Histories_vue__ = __webpack_require__(22);
+var disposed = false
 var normalizeComponent = __webpack_require__(3)
 /* script */
-var __vue_script__ = null
+
 /* template */
-var __vue_template__ = null
+
 /* styles */
 var __vue_styles__ = null
 /* scopeId */
@@ -4335,17 +4339,121 @@ var __vue_scopeId__ = null
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
-  __vue_script__,
-  __vue_template__,
+  __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_Histories_vue___default.a,
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_1e20208e_hasScoped_false_node_modules_vue_loader_lib_selector_type_template_index_0_Histories_vue__["a" /* default */],
   __vue_styles__,
   __vue_scopeId__,
   __vue_module_identifier__
 )
 Component.options.__file = "src/components/Histories.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
+if (Component.options.functional) {console.error("[vue-loader] Histories.vue: functional components are not supported with templates, they should use render functions.")}
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-1e20208e", Component.options)
+  } else {
+    hotAPI.reload("data-v-1e20208e", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
 
 /* harmony default export */ __webpack_exports__["default"] = (Component.exports);
 
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+exports.default = {
+  name: 'histories',
+  data: function data() {
+    return {
+      items: []
+    };
+  },
+
+  methods: {
+    update: function update() {
+      var _this = this;
+
+      chrome.storage.local.get('dmmItems', function (items) {
+        _this.items = items.dmmItems;
+      });
+    }
+  },
+  created: function created() {
+    this.update();
+  }
+};
+
+/***/ }),
+/* 22 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', {
+    staticClass: "container",
+    attrs: {
+      "id": "histories"
+    }
+  }, [_c('h1', [_vm._v("Histories")]), _vm._v(" "), _vm._l((_vm.items), function(item) {
+    return _c('ul', [_c('li', [_c('a', {
+      attrs: {
+        "href": item.href,
+        "target": "_blank"
+      }
+    }, [_c('p', [_vm._v(_vm._s(item.title))]), _vm._v(" "), _c('p', [_c('img', {
+      attrs: {
+        "src": item.imageUrl,
+        "alt": item.title
+      }
+    })]), _vm._v(" "), _c('p', [_vm._v(_vm._s(item.favoriteCount))])])])])
+  })], 2)
+}
+var staticRenderFns = []
+render._withStripped = true
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+     require("vue-hot-reload-api").rerender("data-v-1e20208e", esExports)
+  }
+}
 
 /***/ })
 /******/ ]);
---
+++ src/components/Histories.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="histories" class="container">
+    <h1>Histories</h1>
+
+    <ul v-for="item in items">
+      <li>
+        <a :href="item.href" target="_blank">
+          <p>{{item.title}}</p>
+          <p>
+            <img :src="item.imageUrl" :alt="item.title">
+          </p>
+          <p>{{item.favoriteCount}}</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'histories',
+    data() {
+      return {
+        items: [],
+      }
+    },
+    methods: {
+      update() {
+        chrome.storage.local.get('dmmItems', (items) => {
+          this.items = items.dmmItems;
+        });
+      },
+    },
+    created() {
+      this.update();
+    }
+  }
+</script>
